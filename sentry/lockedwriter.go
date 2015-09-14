818b1570a7f3021fcc8f393bb6e3b35ea6931742
Jeremiah Orem <oremj@oremj.com>
2015-09-14 15:40:51 -0700

    sentry: feature complete

---
+++ sentry/lockedwriter.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"sync"
+)
+
+type lockedWriter struct {
+	buf *bytes.Buffer
+	lck sync.Mutex
+}
+
+func newLockedWriter() *lockedWriter {
+	return &lockedWriter{
+		buf: new(bytes.Buffer),
+		lck: sync.Mutex{},
+	}
+}
+
+func (l *lockedWriter) Printf(format string, args ...interface{}) {
+	l.lck.Lock()
+	defer l.lck.Unlock()
+	l.buf.WriteString(fmt.Sprintf(format, args...))
+}
+
+func (l *lockedWriter) String() string {
+	l.lck.Lock()
+	defer l.lck.Unlock()
+	return l.buf.String()
+}
--- sentry/main.go
+++ sentry/main.go
@@ -3,14 +3,7 @@ package main
 //go:generate echo mm
 
 import (
-	"bytes"
-	"errors"
-	"fmt"
 	"log"
-	"net/http"
-	"net/url"
-	"strings"
-	"time"
 
 	"github.com/codegangsta/cli"
 	"github.com/mozilla-services/go-bouncer/bouncer"
@@ -52,128 +45,8 @@ func Main(c *cli.Context) {
 		log.Fatal(err)
 	}
 
-	sentry.Run()
-}
-
-type Sentry struct {
-	DB        *bouncer.DB
-	locations []*bouncer.LocationsActiveResult
-	mirrors   []*bouncer.MirrorsActiveResult
-	startTime time.Time
-}
-
-func New(db *bouncer.DB, checknow bool, mirror string) (*Sentry, error) {
-	locations, err := db.LocationsActive(checknow)
+	err = sentry.Run()
 	if err != nil {
-		return nil, fmt.Errorf("db.LocationsActive: %v", err)
+		log.Fatal(err)
 	}
-
-	mirrors, err := db.MirrorsActive(mirror)
-	if err != nil {
-		return nil, fmt.Errorf("db.MirrorsActive: %v", err)
-	}
-
-	return &Sentry{
-		DB:        db,
-		locations: locations,
-		mirrors:   mirrors,
-	}, nil
-}
-
-func (s *Sentry) Run() {
-	for _, mirror := range s.mirrors {
-		url, err := url.Parse(mirror.BaseURL)
-		if err != nil {
-			log.Printf("url: %s, err: %v", mirror.BaseURL, err)
-			continue
-		}
-
-		logBuf := new(bytes.Buffer)
-		logBuf.WriteString("Checking mirror " + url.Host + "...\n")
-
-		err := s.CheckMirror(m)
-		if err != nil {
-			dberr := s.DB.MirrorSetHealth(mirror.ID, "0")
-			if dberr != nil {
-				log.Println("MirrorSetHealth:", dberr)
-			}
-			continue
-		}
-	}
-}
-
-func (s *Sentry) CheckMirror(mirror *bouncer.MirrorsActiveResult) error {
-	// Check DNS?
-
-	req, err := http.NewRequest("HEAD", mirror.BaseURL, nil)
-	if err != nil {
-		return err
-	}
-	resp, err := http.DefaultTransport.RoundTrip(req)
-	if err != nil || resp.StatusCode >= 500 {
-		dberr := s.DB.MirrorSetHealth(mirror.ID, "0")
-		if dberr != nil {
-			log.Println(dberr)
-		}
-		if err != nil {
-			dberr := s.DB.SentryLogInsert(s.StartTime, mirror.ID, "0", mirror.Rating, "No Response")
-			if dberr != nil {
-				log.Println(dberr)
-			}
-			return err
-		}
-		err = fmt.Errorf("%s returned %s", mirror.BaseURL, resp.Status)
-		dberr = s.DB.SentryLogInsert(s.StartTime, mirror.ID, "0", mirror.Rating, "Bad Response")
-		if dberr != nil {
-			log.Println(dberr)
-		}
-		return err
-	}
-
-	for _, location := range s.Locations {
-		err := s.CheckLocation(mirror, location)
-		if err != nil {
-			log.Printf("Error checking mirror: %s, location: %s, err: %v", mirror.ID, location.ID, err)
-		}
-	}
-
-	err = s.DB.SentryLogInsert(s.StartTime, mirror.ID, "1", mirror.Rating, logBuf.String())
-	if err != nil {
-		log.Println(err)
-	}
-	return nil
-}
-
-func (s *Sentry) CheckLocation(mirror *bouncer.MirrorsActiveResult, location *bouncer.LocationsActiveResult) error {
-	client := &http.Client{
-		CheckRedirect: func(req *http.Request, via []*http.Request) error {
-			if len(via) >= 1 {
-				return errors.New("Stopped after 1 redirect")
-			}
-			return nil
-		},
-	}
-
-	path := strings.Replace(location.Path, ":lang", "en-US", -1)
-	req, err := http.NewRequest("HEAD", mirror.BaseURL+path, nil)
-	if err != nil {
-		return err
-	}
-	resp, err := client.Do(req)
-	if err != nil {
-		return err
-	}
-
-	active, healthy := "1", "0"
-	if resp.StatusCode == 200 && !strings.Contains(resp.Header.Get("Content-Type"), "text/html") {
-		active, healthy = "1", "1"
-	} else if resp.StatusCode == 404 || resp.StatusCode == 403 {
-		active, healthy = "0", "0"
-	}
-	err = s.DB.MirrorLocationUpdate(location.ID, mirror.ID, active, healthy)
-	if err != nil {
-		return err
-	}
-
-	return nil
 }
