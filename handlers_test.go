68abb41e77c3875126e06bd51e7e38fc01db262f
Jeremiah Orem <oremj@oremj.com>
2015-12-21 09:00:12 -0800

    tests: adjust sha1 product tests

--- handlers_test.go
+++ handlers_test.go
@@ -98,9 +98,9 @@ func TestIsWindowsXPUserAgent(t *testing.T) {
 	}
 }
 
-func TestFirefoxSha1Product(t *testing.T) {
-	assert.Equal(t, "firefox-43.0.1-SSL", firefoxSha1Product("firefox-latest"))
-	assert.Equal(t, "firefox-43.0.1-SSL", firefoxSha1Product("firefox"))
-	assert.Equal(t, "firefox-43.0.1-SSL", firefoxSha1Product("firefox-44.0.0-SSL"))
-	assert.Equal(t, "firefox-42.0.0-SSL", firefoxSha1Product("firefox-42.0.0-SSL"))
+func TestSha1Product(t *testing.T) {
+	assert.Equal(t, "firefox-43.0.1-SSL", sha1Product("firefox-latest"))
+	assert.Equal(t, "firefox-43.0.1-SSL", sha1Product("firefox"))
+	assert.Equal(t, "firefox-43.0.1-SSL", sha1Product("firefox-44.0.0-SSL"))
+	assert.Equal(t, "firefox-42.0.0-SSL", sha1Product("firefox-42.0.0-SSL"))
 }
