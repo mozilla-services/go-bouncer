3cf18813ae146664f1897d769cdd9ad84ade244f
Jeremiah Orem <oremj@oremj.com>
2016-10-24 13:39:48 -0700

    add attribution_sig to handlers_test

--- handlers_test.go
+++ handlers_test.go
@@ -34,14 +34,14 @@ func TestBouncerHandlerAttributionCode(t *testing.T) {
 	w := httptest.NewRecorder()
 
 	req, err := http.NewRequest("GET",
-		"http://test/?product=Firefox&os=win&lang=en-US&attribution_code=source%3Dgoogle.com%26medium%3Dorganic%26campaign%3D(not%20set)%26content%3D(not%20set)",
+		"http://test/?product=Firefox&os=win&lang=en-US&attribution_code=source%3Dgoogle.com%26medium%3Dorganic%26campaign%3D(not%20set)%26content%3D(not%20set)&attribution_sig=anhmacsig",
 		nil)
 	assert.NoError(t, err)
 
 	bouncerHandler.ServeHTTP(w, req)
 	assert.Equal(t, 302, w.Code)
 	assert.Equal(t,
-		"https://stub/?attribution_code=source%3Dgoogle.com%26medium%3Dorganic%26campaign%3D%28not+set%29%26content%3D%28not+set%29&lang=en-US&os=win&product=firefox",
+		"https://stub/?attribution_code=source%3Dgoogle.com%26medium%3Dorganic%26campaign%3D%28not+set%29%26content%3D%28not+set%29&attribution_sig=anhmacsig&lang=en-US&os=win&product=firefox",
 		w.HeaderMap.Get("Location"))
 }
 
