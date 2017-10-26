b17a31c88757c2163e988fa296e439ac0bb1cf0b
Dave Hunt <dave.hunt@gmail.com>
2017-10-26 16:27:52 +0100

    Add flake8-isort to check import sort order

--- tests/e2e/tests/test_redirects.py
+++ tests/e2e/tests/test_redirects.py
@@ -2,13 +2,14 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this file,
 # You can obtain one at http://mozilla.org/MPL/2.0/.
 
-import pytest
-import requests
 from urllib import urlencode
 from urlparse import urlparse
 
-from base import Base
-import releng_utils as utils
+import pytest
+import requests
+
+from . import releng_utils as utils
+from .base import Base
 
 
 class TestRedirects(Base):
