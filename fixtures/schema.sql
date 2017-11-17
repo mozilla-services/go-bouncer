e6e71ee497897cd47c136311572dafc63ae6524d
Jeremiah Orem <oremj@oremj.com>
2017-11-17 09:09:52 -0800

    Update schema.sql

--- fixtures/schema.sql
+++ fixtures/schema.sql
@@ -232,7 +232,7 @@ CREATE TABLE `mirror_locations` (
   `product_id` int(11) NOT NULL DEFAULT '0',
   `os_id` int(11) NOT NULL DEFAULT '0',
   `path` varchar(255) NOT NULL DEFAULT '',
-  PRIMARY KEY (`id`,`product_id`,`os_id`)
+  PRIMARY KEY (`id`,`product_id`,`os_id`),
   KEY `product_os_idx` (`product_id`,`os_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=23194 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
